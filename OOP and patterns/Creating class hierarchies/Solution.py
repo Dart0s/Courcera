e0b0c36ae2977bd06c423f1bf451963a1d308d35
Вячеслав Богатырев <slava-2016@yandex.ru>
2020-08-06 19:22:08 +0300

    Week 2 part 2
    Stub software architecture

---
+++ OOP and patterns/Creating class hierarchies/Solution.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+import math
+
+SCREEN_DIM = (800, 600)
+
+
+class Vec2d:
+    pass
+
+
+class Polyline:
+    pass
+
+
+class Knot(Polyline):
+    pass
+
+
+if __name__ == "__main__":
+    pygame.init()
+    gameDisplay = pygame.display.set_mode(SCREEN_DIM)
+    pygame.display.set_caption("MyScreenSaver")
+
